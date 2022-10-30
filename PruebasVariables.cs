3ef096d688a3c7ddbdbc7c9a167724ecc63ad4aa
Javier Jiménez Sanz <104020906+Himmanchu@users.noreply.github.com>
2022-10-30 09:57:06 +0100

    Commit

---
+++ PruebasVariables.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebasVariables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //crear variables
+        int numero1;
+        int numero2;
+        string nombre;
+        int vidas;
+
+
+        //Asignar valores
+        numero1 = 10;
+        nombre = "Jimenotti";
+        vidas= 5;
+        numero2 = 36;
+
+        Debug.Log(vidas);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
